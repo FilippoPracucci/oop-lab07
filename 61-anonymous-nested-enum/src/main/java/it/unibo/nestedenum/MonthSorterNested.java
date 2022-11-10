bb4dfb8fcbb45c560b8046f6adc1f0bdb7921e29
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2022-11-10 18:25:23 +0100

    Add the enum Month

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,50 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    private enum Month {
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(final int days) {
+            this.days = days;
+        }
+
+        Month fromString(final String monthName) {
+            Objects.requireNonNull(monthName);
+            try {
+                return valueOf(monthName);
+            } catch (IllegalArgumentException e) {
+                Month monthMatch = null;
+                for (final Month month : values()) {
+                    if (month.toString().toLowerCase(Locale.ROOT).startsWith(monthName.toLowerCase(Locale.ROOT))) {
+                        if (monthMatch != null) {
+                            throw new IllegalArgumentException(
+                                "Ambiguity for month " + monthName + ". Can be: " + monthMatch + ", and " + month, e
+                            );
+                        }
+                        monthMatch = month;
+                    }
+                }
+                if (monthMatch == null) {
+                    throw new IllegalArgumentException("No match for month " + monthName, e);
+                }
+                return monthMatch;
+            }
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
