7989d37242301b75d2f1a641eb9ae8d7a5e95dac
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2022-11-11 18:47:45 +0100

    Part 2 done

--- 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -4,6 +4,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -38,6 +40,11 @@ public class MiniGUI {
         canvas.add(panel, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
         panel.add(write);
+        /*
+         * Part 2
+         */
+        final JTextField resultField = new JTextField("Result");
+        canvas.add(resultField, BorderLayout.NORTH);
 
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
