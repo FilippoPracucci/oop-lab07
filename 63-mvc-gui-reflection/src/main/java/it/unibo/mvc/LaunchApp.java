4cb8cf0c9602afe78b3d60edd8397d8e4f19822b
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2022-11-12 16:10:41 +0100

    Implementation of views in LaunchApp; ex. finished

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,15 +1,19 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberStdoutView;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
  */
 public final class LaunchApp {
+
+    private final static int N_VIEWS = 3;
 
     private LaunchApp() { }
 
@@ -24,10 +28,28 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws
+        ClassNotFoundException,
+        InstantiationException,
+        IllegalAccessException,
+        IllegalArgumentException,
+        InvocationTargetException,
+        NoSuchMethodException {
+
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberStdoutView());
+        for (final String name : List.of("Swing", "Stdout")) {
+            final Class<?> viewClass = Class.forName("it.unibo.mvc.view.DrawNumber" + name + "View");
+            for (int i = 0; i < N_VIEWS; i++) {
+                final Object newClass = viewClass.getConstructor().newInstance();
+                if (DrawNumberView.class.isAssignableFrom(newClass.getClass())) {
+                    app.addView((DrawNumberView) newClass);
+                } else {
+                    throw new IllegalStateException (
+                        newClass.getClass() + " view is not a subclass of " + DrawNumberView.class
+                    );
+                }
+            }
+        }
     }
 }
